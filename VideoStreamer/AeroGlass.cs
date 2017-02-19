02c5867c92173d05495abf230c398afb0c6239b8
Luuk Diederik <flipmil3@hotmail.com>
2017-02-19 23:05:02 +0100

    Fixed maximize resolution shift

--- VideoStreamer/AeroGlass.cs
+++ VideoStreamer/AeroGlass.cs
@@ -1,44 +1,273 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Interop;
+using System.Runtime.InteropServices;
+using Application = System.Windows.Application;
+using Panel = System.Windows.Controls.Panel;
 
 namespace VideoStreamer {
 
     public partial class MainWindow : Window {
+        private void AddUIElementToPanel(UIElement uiElement, Panel panel, int rowIndex = 0, int columnIndex = 0) {
+            Grid.SetRow(uiElement, rowIndex);
+            Grid.SetColumn(uiElement, columnIndex);
+            panel.Children.Add(uiElement);
+        }
+
         private void Window_Loaded (object sender, RoutedEventArgs e) {
             AeroGlass.EnableBlur(this);
         }
 
-        private Point _mousePos;
-
-        private void Rectangle_MouseLeftButtonDown (object sender, MouseButtonEventArgs e) {
-            Window window = Application.Current.MainWindow;
-
+        private void WindowBar_MouseLeftButtonDown (object sender, MouseButtonEventArgs e) {
             if (e.ChangedButton != MouseButton.Left) return;
 
-            if (window.WindowState == WindowState.Maximized) {
-                window.WindowState = WindowState.Normal;
-                window.Left = _mousePos.X - window.Width / 2;
-                window.Top = _mousePos.Y - 20;
+            if (WindowState == WindowState.Maximized) {
+                RestoreWindow();
             }
 
-            window.DragMove();
+            DragMove();
         }
 
-        private void Window_LocationChanged (object sender, EventArgs e) {
+        private void Window_StateChanged(object sender, EventArgs e) {
+            if (WindowState == WindowState.Maximized)
+                MaximizeWindow();
+            else
+                RestoreWindow();
+        }
+
+        private void RestoreWindow() {
             Window window = Application.Current.MainWindow;
 
-            _mousePos = Mouse.GetPosition(this);
-            label.Content = "Mouse X: " + PointToScreen(_mousePos).X + "\nMouse Y: " + PointToScreen(_mousePos).Y;
+            MaxHeight = double.PositiveInfinity;
+            MaxWidth = double.PositiveInfinity;
+            Left = System.Windows.Forms.Cursor.Position.X - Width / 2;
+            Top = System.Windows.Forms.Cursor.Position.Y - WindowBar.Height / 2;
+            
+            window.WindowState = WindowState.Normal;
+        }
+
+        private void MaximizeWindow() {
+            Window window = Application.Current.MainWindow;
+
+            System.Drawing.Point pt = System.Windows.Forms.Cursor.Position;
+            var currentScreen = Screen.FromPoint(pt);
+            
+            if (currentScreen.Primary) {
+                MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+                MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+
+                Width = 1280;
+                Height = 720;
+                Left = 0;
+                Top = 0;
+            } else {
+                MaxHeight = double.PositiveInfinity;
+                MaxWidth = double.PositiveInfinity;
+
+                Width = 1280;
+                Height = 720;
+                Left = currentScreen.Bounds.X;
+                Top = currentScreen.Bounds.Y;
+            }
+            
+            window.WindowState = WindowState.Maximized;
         }
     }
 
+    // Crazy shit, don't touch.
+    public partial class MainWindow : Window {
+        private static IntPtr WindowProc(
+              IntPtr hwnd,
+              int msg,
+              IntPtr wParam,
+              IntPtr lParam,
+              ref bool handled) {
+            switch (msg) {
+                case 0x0024:
+                    WmGetMinMaxInfo(hwnd, lParam);
+                    handled = true;
+                    break;
+            }
+
+            return (IntPtr)0;
+        }
+
+        private static void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam) {
+
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+
+            // Adjust the maximized size and position to fit the work area of the correct monitor
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            if (monitor != IntPtr.Zero) {
+
+                MONITORINFO monitorInfo = new MONITORINFO();
+                GetMonitorInfo(monitor, monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+            }
+
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+
+
+        /// <summary>
+        /// POINT aka POINTAPI
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT {
+            /// <summary>
+            /// x coordinate of point.
+            /// </summary>
+            public int x;
+            /// <summary>
+            /// y coordinate of point.
+            /// </summary>
+            public int y;
+
+            /// <summary>
+            /// Construct a point of coordinates (x,y).
+            /// </summary>
+            public POINT(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        }
+        
+        /// <summary>
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO {
+            /// <summary>
+            /// </summary>            
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+
+            /// <summary>
+            /// </summary>            
+            public RECT rcMonitor = new RECT();
+
+            /// <summary>
+            /// </summary>            
+            public RECT rcWork = new RECT();
+
+            /// <summary>
+            /// </summary>            
+            public int dwFlags = 0;
+        }
+        
+        /// <summary>
+        /// Win32
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct RECT {
+            /// <summary> Win32 </summary>
+            public int left;
+            /// <summary> Win32 </summary>
+            public int top;
+            /// <summary> Win32 </summary>
+            public int right;
+            /// <summary> Win32 </summary>
+            public int bottom;
+
+            /// <summary> Win32 </summary>
+            public static readonly RECT Empty = new RECT();
+
+            /// <summary> Win32 </summary>
+            public int Width {
+                get { return Math.Abs(right - left); }  // Abs needed for BIDI OS
+            }
+            /// <summary> Win32 </summary>
+            public int Height {
+                get { return bottom - top; }
+            }
+
+            /// <summary> Win32 </summary>
+            public RECT(int left, int top, int right, int bottom) {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+
+
+            /// <summary> Win32 </summary>
+            public RECT(RECT rcSrc) {
+                this.left = rcSrc.left;
+                this.top = rcSrc.top;
+                this.right = rcSrc.right;
+                this.bottom = rcSrc.bottom;
+            }
+
+            /// <summary> Win32 </summary>
+            public bool IsEmpty {
+                get {
+                    // BUGBUG : On Bidi OS (hebrew arabic) left > right
+                    return left >= right || top >= bottom;
+                }
+            }
+            /// <summary> Return a user friendly representation of this struct </summary>
+            public override string ToString() {
+                if (this == RECT.Empty) { return "RECT {Empty}"; }
+                return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom + " }";
+            }
+
+            /// <summary> Determine if 2 RECT are equal (deep compare) </summary>
+            public override bool Equals(object obj) {
+                if (!(obj is Rect)) { return false; }
+                return (this == (RECT)obj);
+            }
+
+            /// <summary>Return the HashCode for this struct (not garanteed to be unique)</summary>
+            public override int GetHashCode() {
+                return left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
+            }
+
+
+            /// <summary> Determine if 2 RECT are equal (deep compare)</summary>
+            public static bool operator ==(RECT rect1, RECT rect2) {
+                return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom);
+            }
+
+            /// <summary> Determine if 2 RECT are different(deep compare)</summary>
+            public static bool operator !=(RECT rect1, RECT rect2) {
+                return !(rect1 == rect2);
+            }
+
+
+        }
+
+        [DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DllImport("User32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
+    }
+
+    // More crazy shit, don't touch.
     internal static class AeroGlass {
         [DllImport("user32.dll")]
         internal static extern int SetWindowCompositionAttribute (IntPtr hwnd, ref WindowCompositionAttributeData data);
--- VideoStreamer/MainWindow.xaml.cs
+++ VideoStreamer/MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using WinInterop = System.Windows.Interop;
+using System.Runtime.InteropServices;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -24,8 +26,17 @@ namespace VideoStreamer {
 
         public MainWindow() {
             InitializeComponent();
+            SourceInitialized += win_SourceInitialized;
+
             GetSearch("the walking dead");
         }
+
+        void win_SourceInitialized(object sender, EventArgs e) {
+            IntPtr handle = new WinInterop.WindowInteropHelper(this).Handle;
+            WinInterop.HwndSource.FromHwnd(handle).AddHook(WindowProc);
+        }
+
+        List<Label> labelList = new List<Label>();
 
         private void GetSearch(string search, int page = 0, int category = TorrentCategory.All, QueryOrder queryOrder = QueryOrder.BySeeds) {
             List<Torrent> torrentList = Tpb.Search(
@@ -37,12 +48,24 @@ namespace VideoStreamer {
                 )
             ).ToList();
 
+            foreach (Label label in labelList)
+                WindowContent.Children.Remove(label);
+
+            labelList.Clear();
             foreach (Torrent t in torrentList) {
-                label.Content = t.File;
+                labelList.Add(
+                    new Label {
+                        Content = t.Name,
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Top,
+                        Margin = new Thickness(10, labelList.Count * 35, 0, 0),
+                        Foreground = Brushes.White,
+                        FontWeight = FontWeight.FromOpenTypeWeight(labelList.Count == 0 ? 1 : 999)
+                    }
+                );
+
+                AddUIElementToPanel(labelList[labelList.Count - 1], WindowContent);
             }
-
-            label.Content = torrentList[0].Name + "\nOK";
         }
-
     }
 }
